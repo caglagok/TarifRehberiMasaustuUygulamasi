c4946caec506d1e301ffae95d9f38b7715418628
keyiktugba <114024460+keyiktugba@users.noreply.github.com>
2024-10-07 21:53:19 +0300

    Proje dosyası ekle.

---
+++ Yazlab_1/Malzemeler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yazlab_1
+{
+    internal class Malzemeler
+    {
+        public int MalzemeID { get; set; }   
+        public string MalzemeAdi { get; set; }   
+        public string ToplamMiktar { get; set; } 
+        public string MalzemeBirim { get; set; }   
+        public decimal BirimFiyat { get; set; }
+    }
+}
---
+++ Yazlab_1/Tarifler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yazlab_1
+{
+    internal class Tarifler
+    {
+        public int TarifID { get; set; }   
+        public string TarifAdi { get; set; }  
+        public string Kategori { get; set; } 
+        public int HazirlamaSuresi { get; set; }  
+        public string Talimatlar { get; set; }   
+        public List<Malzemeler> Malzemeler { get; set; }   
+    }
+}
