9676d4ee9bef1cd72741ddd189f2210d783aae32
Çağla <114026286+caglagok@users.noreply.github.com>
2024-10-18 15:34:20 +0300

    video
    video

--- Yazlab_1/Baslangıc.Designer.cs
+++ Yazlab_1/Baslangıc.Designer.cs
@@ -29,13 +29,15 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            panel1 = new Panel();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
             button1.BackColor = Color.IndianRed;
             button1.Font = new Font("Century", 13F);
-            button1.Location = new Point(980, 440);
+            button1.Location = new Point(1152, 742);
             button1.Name = "button1";
             button1.Size = new Size(94, 42);
             button1.TabIndex = 0;
@@ -43,22 +45,33 @@
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Location = new Point(0, -3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1262, 800);
+            panel1.TabIndex = 1;
+            //panel1.Paint += panel1_Paint;
+            // 
             // Baslangıc
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.Linen;
             ClientSize = new Size(1258, 793);
-            Controls.Add(button1);
+            Controls.Add(panel1);
             Name = "Baslangıc";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Baslangıc";
             Load += Baslangıc_Load;
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Button button1;
+        private Panel panel1;
     }
 }
--- Yazlab_1/Baslangıc.cs
+++ Yazlab_1/Baslangıc.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibVLCSharp.Shared;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,15 +13,16 @@ namespace Yazlab_1
 {
     public partial class Baslangıc : Form
     {
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
         public Baslangıc()
         {
             InitializeComponent();
+            Core.Initialize(); // LibVLC motoru
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
-
             Ana_Sayfa anasayfaForm = new Ana_Sayfa();
             anasayfaForm.ShowDialog();
 
@@ -29,7 +31,31 @@ namespace Yazlab_1
 
         private void Baslangıc_Load(object sender, EventArgs e)
         {
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
 
+            // VideoView'i oluştur ve panele ekle
+            var videoView = new LibVLCSharp.WinForms.VideoView
+            {
+                Dock = DockStyle.Fill
+            };
+            panel1.Controls.Add(videoView);
+            videoView.MediaPlayer = _mediaPlayer;
+
+            // Video yolunu ayarla
+            var videoPath = @"C:\Users\CAGLA\Desktop\Yazlab1\Yazlab_1\Yazlab_1\TarifResimleri\baslangic4.mp4";
+            var media = new Media(_libVLC, new Uri(videoPath));
+
+            // Videoyu oynat
+            _mediaPlayer.Play(media);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Uygulama kapanırken kaynakları serbest bırakma
+            _mediaPlayer?.Dispose();
+            _libVLC?.Dispose();
+            base.OnFormClosing(e);
         }
     }
 }
